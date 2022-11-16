922394af42e8736a0d3dc36620feb3d6faafbfb3
S <sushantdhote09876@outlook.com>
2022-11-16 10:43:27 +0530

    Remove password from a directory of files

---
+++ code.py
@@ -0,0 +1,40 @@
+import pikepdf
+import os
+import shutil
+
+# Load Wordlist
+wordlist = input('Enter wordlist: ')
+passwords = passwords = [ line.strip() for line in open(wordlist) ]
+
+# Making Directory
+try:
+    os.mkdir('Decrypted')
+except FileExistsError as e:
+    pass
+
+# iterate over passwords
+def crack(p):
+ if ".pdf" in str(p):
+  #print('\nOpening: '+str(p))
+  print(str(p))
+  for password in passwords:
+   try:
+    # open PDF file
+    with pikepdf.open(p, password=password) as pdf:
+     # Password decrypted successfully, break out of the loop
+     print("[+] Password found:", password)
+     x=i.split('.pdf')[0]
+     y=x+"_DECRYPTED.pdf"
+     pdf.save(y)
+     shutil.move(y, 'Decrypted')
+     pdf.close()
+     #os.remove(i)
+     break
+   except pikepdf._qpdf.PasswordError as e:
+    # wrong password, just continue in the loop
+    continue
+ else:
+  pass
+
+for i in os.listdir(os.getcwd()):
+ crack(i)
